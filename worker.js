9fd6cebab151b20abd2daaa3fe73c3c66321dae7
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-04-22 00:20:15 +0700

    Fix lint

--- worker.js
+++ worker.js
@@ -14,10 +14,10 @@ const inputSensorData = async () => {
     ids.map(async id => {
       const payload = [
         id.id,
-        Math.floor(Math.random() * 50),           // C
-        Math.floor(Math.random() * 100),          // %
-        Math.floor(Math.random() * 5000) + 5000,  // Cd/m2
-        Math.floor(Math.random() * 10),           // m/s
+        Math.floor(Math.random() * 50), // C
+        Math.floor(Math.random() * 100), // %
+        Math.floor(Math.random() * 5000) + 5000, // Cd/m2
+        Math.floor(Math.random() * 10), // m/s
         cuacaEnum[Math.floor(Math.random() * 3)]
       ]
       await client.query(`INSERT INTO data_sensor (id_lahan, suhu, kelembaban, cahaya, angin, cuaca, waktu)
